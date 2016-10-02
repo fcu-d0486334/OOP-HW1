947a21e275a3e53229c9447306a747ded901c365
Eugene <joylemon112233@gmail.com>
2016-10-02 20:58:59 +0800

    9/30作業

--- src/fcu/iecs/oop/Hello.java
+++ src/fcu/iecs/oop/Hello.java
@@ -1,10 +1,12 @@
 package fcu.iecs.oop;
 
-public class Hello {
-
-	public static void main(String[] args) {
-		System.out.println("Hello OOP");
-
+public class Hello{
+	public static void main(String args[]){
+		StringBuffer lyrics=new StringBuffer("Let it go! Let it go! Cannot hold it back anymore");
+		lyrics.replace(4,6,"\"her\"");
+		lyrics.replace(18,20,"\"her\"");
+		lyrics.replace(28,34,"Can't");
+		lyrics.replace(39,41,"\"her\"");
+		System.out.println(lyrics);
 	}
-
 }
